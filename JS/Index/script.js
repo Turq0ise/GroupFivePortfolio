97ade90df4aadc40d06d6acefe3dd626bdc70bd4
Turq0ise <72288865+Turq0ise@users.noreply.github.com>
2021-10-19 09:54:46 +0800

    UpDaTeS!?!?!?!?!

--- JS/Index/script.js
+++ JS/Index/script.js
@@ -1,18 +1,33 @@
 function btnDateChange() {
-    const ctaBtn = document.getElementById("cta-btn")
+    const contentDiv = document.getElementById("content")
     var date = new Date()
     var month = date.getMonth() + 1
     var year = date.getFullYear()
 
     if(month == 10 && year == 2021) {
-        ctaBtn.innerHTML += "<a href=PROJECTS/Q1><button class=main-cta-btn>Q1</button></a>"
+        contentDiv.innerHTML += "<a href=PROJECTS/Q1 id=cta-btn><button class=main-cta-btn>Q1</button></a>"
     } else if(month == 11 || month == 12 && year == 2021) {
-        ctaBtn.innerHTML += "<a href=PROJECTS/Q2><button class=main-cta-btn>Q2</button></a>"
+        contentDiv.innerHTML += "<a href=PROJECTS/Q2 id=cta-btn><button class=main-cta-btn>Q2</button></a>"
     } else if(month == 1 || month == 2 && year == 2022) {
-        ctaBtn.innerHTML += "<a href=PROJECTS/Q3><button class=main-cta-btn>Q3</button></a>"
+        contentDiv.innerHTML += "<a href=PROJECTS/Q3 id=cta-btn><button class=main-cta-btn>Q3</button></a>"
     } else if(month >= 3 && month <= 5 && year == 2022) {
-        ctaBtn.innerHTML += "<a href=PROJECTS/Q4><button class=main-cta-btn>Q4</button></a>"
+        contentDiv.innerHTML += "<a href=PROJECTS/Q4 id=cta-btn><button class=main-cta-btn>Q4</button></a>"
     } else {
-        ctaBtn.innerHTML += "<a href=PROJECTS><button class=main-cta-btn>Projects</button></a>"
+        contentDiv.innerHTML += "<a href=PROJECTS id=cta-btn><button class=main-cta-btn>Projects</button></a>"
     }
 }
+
+var mainForm = document.querySelector("#main-form");
+
+mainForm.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    const form = e.target
+    var data = new FormData(form)
+
+    fetch(form.action, {
+        method: form.method,
+        body: data
+    }).then(
+        alert("Form Submitted")
+    )
+})
